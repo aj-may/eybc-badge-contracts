a8c5b6f6f70c9a6b3c6eba78c43f7723d31b3f54
AJ May <aj7may@gmail.com>
2022-08-28 11:17:16 -0400

    swap Ownable for AccessControl and update metadata urls

--- contracts/EYBlockchainBadge.sol
+++ contracts/EYBlockchainBadge.sol
@@ -4,32 +4,39 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract EYBlockchainBadge is ERC721, ERC721Enumerable, ERC721Burnable, Ownable, EIP712, ERC721Votes {
+contract EYBlockchainBadge is ERC721, ERC721Enumerable, ERC721Burnable, AccessControlEnumerable, EIP712, ERC721Votes {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    bytes32 public constant SYSTEM_ROLE = keccak256("SYSTEM_ROLE");
 
     constructor()
         ERC721("EY Blockchain Badge", "EYBB")
         EIP712("EY Blockchain Badge", "1")
-    {}
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://badge.eybc.xyz/api/badges/";
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function issue(address to) public onlyOwner {
+    function contractURI() public pure returns (string memory) {
+        return "https://badge.eybc.xyz/metadata/";
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://badge.eybc.xyz/metadata/";
+    }
+
+    function issue(address to) public onlyRole(SYSTEM_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
 
-    function revoke(uint256 tokenId) public onlyOwner {
+    function revoke(uint256 tokenId) public onlyRole(SYSTEM_ROLE) {
         _burn(tokenId);
     }
 
@@ -53,7 +60,7 @@ contract EYBlockchainBadge is ERC721, ERC721Enumerable, ERC721Burnable, Ownable,
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, AccessControlEnumerable)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
